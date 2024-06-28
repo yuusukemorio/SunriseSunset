113770f881868324cb697fd68e55e99c8b87d944
MorioYusuke <morio@MorioYusukenoMacBook-Pro.local>
2024-06-28 15:54:53 +0900

    元に戻す

--- index_js.js
+++ index_js.js
@@ -4,38 +4,38 @@ window.onload = function() {
 
     function updateWindowPosition() {
         if (test) {
-            // ウィンドウのx, y位置を取得
             const x = window.screenX;
             const y = window.screenY;
-            // 画面の幅と高さを取得
             const screenWidth = window.innerWidth;
             const screenHeight = window.innerHeight;
-            // pタグの内容にx, y位置を上書き
-            // test.textContent = `ウィンドウの位置 - X: ${x}, Y: ${y}`;
-            // pタグの位置をウィンドウの反対方向に合わせて更新
             test.style.left = `${screenWidth - x}px`;
             test.style.top = `${screenHeight - y}px`;
         }
         if (test2) {
-            // ウィンドウのx, y位置を取得
             const x2 = window.screenX;
             const y2 = window.screenY;
-            // 画面の幅と高さを取得
             const screenWidth2 = window.innerWidth;
             const screenHeight2 = window.innerHeight;
-            // pタグの内容にx, y位置を上書き
-            // test.textContent = `ウィンドウの位置 - X: ${x}, Y: ${y}`;
-            // pタグの位置をウィンドウの反対方向に合わせて更新
             test2.style.left = `${screenWidth2 - x2 + 400}px`;
             test2.style.top = `${screenHeight2 - y2 + 300}px`;
         }
     }
 
-    
-
-    // 初回実行
     updateWindowPosition();
-
-    // 1秒ごとにウィンドウの位置を更新
     setInterval(updateWindowPosition, 0.01);
+
+    const latitude = 34.573; // 堺市の緯度
+    const longitude = 135.483; // 堺市の経度
+    const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&formatted=0`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const sunrise = new Date(data.results.sunrise);
+            const sunset = new Date(data.results.sunset);
+
+            document.getElementById('sunrise').textContent = `日の出: ${sunrise.toLocaleTimeString()}`;
+            document.getElementById('sunset').textContent = `日の入り: ${sunset.toLocaleTimeString()}`;
+        })
+        .catch(error => console.error('Error fetching sunrise and sunset data:', error));
 };
